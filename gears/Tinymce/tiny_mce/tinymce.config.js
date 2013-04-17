b7f02d6cb6a327e1de3f39ba48b96345c886cc7a
yaredlex <ya.redlex@yandex.ru>
2013-04-17 20:34:34 +0300

    Оптимизация еллементов редактирования
    Перегруппированы блоки кнопок.

--- gears/Tinymce/tiny_mce/tinymce.config.js
+++ gears/Tinymce/tiny_mce/tinymce.config.js
@@ -9,31 +9,27 @@
 	tinyMCE.init({
        // General options
 	   language : "ru",
-        mode : "textareas",
-        theme : "advanced",
-        plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-	forced_root_block : false,
-	force_br_newlines : true,
-	force_p_newlines : false,
-        // Theme options
-        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+     mode : "textareas",
+     theme : "advanced",
+     forced_root_block : false,
+  	 force_br_newlines : true,
+		 force_p_newlines : false,
+		 plugins : "style,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+	        // Theme options
+         theme_advanced_buttons3 : "bold,italic,underline,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,outdent,indent,|,blockquote,forecolor,backcolor,strikethrough,charmap,|,link,unlink,anchor,|,sub,sup,visualchars,|,undo,redo,|,code",  
+        theme_advanced_buttons2 : "formatselect,styleselect,fontselect,fontsizeselect,cut,copy,paste,pastetext,pasteword,|,image,emotions,iespell,media,insertdate,inserttime,advhr,search,replace",
+        theme_advanced_buttons1 : "tablecontrols,|,hr,removeformat,visualaid,ltr,rtl,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,nonbreaking,cleanup,template",
         theme_advanced_toolbar_location : "top",
         theme_advanced_toolbar_align : "left",
         theme_advanced_statusbar_location : "bottom",
         theme_advanced_resizing : true,
-
         // Example content CSS (should be your site CSS)
         content_css : "../../../cache/assets/styles.css",
-
         // Drop lists for link/image/media/template dialogs
         template_external_list_url : "js/template_list.js",
         external_link_list_url : "js/link_list.js",
         external_image_list_url : "js/image_list.js",
         media_external_list_url : "js/media_list.js",
-
         // Replace values for the template plugin
         template_replace_values : {
                 username : "Some User",
